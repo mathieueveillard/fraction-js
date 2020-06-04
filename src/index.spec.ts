1e2c639aa4e9339213b51a7d360c4587fcf264d2
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2020-06-04 21:40:12 +0200

    Version 1

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,56 @@
-import { doTheThing } from ".";
+import { Fraction } from ".";
 
-describe("Test of the test framework", function() {
-  it("Should pass", function() {
-    expect(doTheThing()).toEqual(true);
+describe("Fraction", function() {
+  it("Should be able to add 0 to a fraction", function() {
+    const f1 = new Fraction(1, 1);
+    const f2 = new Fraction(0, 1);
+    const actual = f1.plus(f2);
+    const expected = f1;
+    expect(actual).toEqual(expected);
+  });
+
+  it("Should be able to add two fractions with the same denominator", function() {
+    const f1 = new Fraction(1, 1);
+    const f2 = new Fraction(2, 1);
+    const actual = f1.plus(f2);
+    const expected = new Fraction(3, 1);
+    expect(actual).toEqual(expected);
+  });
+
+  it("Should be able to multiply a fraction", function() {
+    const f = new Fraction(1, 2);
+    const actual = f.times(2);
+    const expected = new Fraction(2, 4);
+    expect(actual).toEqual(expected);
+  });
+
+  it("Should be able to add fractions with different denominators", function() {
+    const f1 = new Fraction(1, 2);
+    const f2 = new Fraction(1, 3);
+    const actual = f1.plus(f2);
+    const expected = new Fraction(5, 6);
+    expect(actual).toEqual(expected);
+  });
+
+  it("Should be able to divide a fraction", function() {
+    const f = new Fraction(2, 4);
+    const actual = f.dividedBy(2);
+    const expected = new Fraction(1, 2);
+    expect(actual).toEqual(expected);
+  });
+
+  it("Should be able to reduce a fraction", function() {
+    const f = new Fraction(6, 9);
+    const actual = f.reduce();
+    const expected = new Fraction(2, 3);
+    expect(actual).toEqual(expected);
+  });
+
+  it("Should be able to add fractions with different denominators and reduce the resulting fraction", function() {
+    const f1 = new Fraction(1, 4);
+    const f2 = new Fraction(1, 4);
+    const actual = f1.plus(f2);
+    const expected = new Fraction(1, 2);
+    expect(actual).toEqual(expected);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-export function doTheThing(): boolean {
-  return true;
+import { gcd } from "./gcd";
+
+export class Fraction {
+  constructor(private numerator: number, private denominator: number) {}
+
+  times(value: number): Fraction {
+    return new Fraction(this.numerator * value, this.denominator * value);
+  }
+
+  dividedBy(value: number): Fraction {
+    return new Fraction(this.numerator / value, this.denominator / value);
+  }
+
+  reduce(): Fraction {
+    return this.dividedBy(gcd(this.numerator, this.denominator));
+  }
+
+  plus(other: Fraction): Fraction {
+    const f1 = this.times(other.denominator);
+    const f2 = other.times(this.denominator);
+    return new Fraction(f1.numerator + f2.numerator, f1.denominator).reduce();
+  }
 }
