26a278e57a525096ca7d4a17b7b0e284d3976b9f
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2021-03-13 12:10:08 +0100

    Exploration with defensive programming

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,56 +1,62 @@
-import { Fraction } from ".";
+import { Denominator, Fraction, Numerator } from ".";
+
+describe("Denominator", function() {
+  it("Should NOT allow to create a zero denominator", function() {
+    expect(() => new Denominator(0)).toThrowError("Cannot create a zero denominator.");
+  });
+});
 
 describe("Fraction", function() {
   it("Should be able to add 0 to a fraction", function() {
-    const f1 = new Fraction(1, 1);
-    const f2 = new Fraction(0, 1);
+    const f1 = new Fraction(new Numerator(1), new Denominator(1));
+    const f2 = new Fraction(new Numerator(0), new Denominator(1));
     const actual = f1.plus(f2);
     const expected = f1;
     expect(actual).toEqual(expected);
   });
 
   it("Should be able to add two fractions with the same denominator", function() {
-    const f1 = new Fraction(1, 1);
-    const f2 = new Fraction(2, 1);
+    const f1 = new Fraction(new Numerator(1), new Denominator(1));
+    const f2 = new Fraction(new Numerator(2), new Denominator(1));
     const actual = f1.plus(f2);
-    const expected = new Fraction(3, 1);
+    const expected = new Fraction(new Numerator(3), new Denominator(1));
     expect(actual).toEqual(expected);
   });
 
   it("Should be able to scale up a fraction", function() {
-    const f = new Fraction(1, 2);
+    const f = new Fraction(new Numerator(1), new Denominator(2));
     const actual = f.scaleByMultiplication(2);
-    const expected = new Fraction(2, 4);
+    const expected = new Fraction(new Numerator(2), new Denominator(4));
     expect(actual).toEqual(expected);
   });
 
   it("Should be able to add fractions with different denominators", function() {
-    const f1 = new Fraction(1, 2);
-    const f2 = new Fraction(1, 3);
+    const f1 = new Fraction(new Numerator(1), new Denominator(2));
+    const f2 = new Fraction(new Numerator(1), new Denominator(3));
     const actual = f1.plus(f2);
-    const expected = new Fraction(5, 6);
+    const expected = new Fraction(new Numerator(5), new Denominator(6));
     expect(actual).toEqual(expected);
   });
 
   it("Should be able to scale down a fraction", function() {
-    const f = new Fraction(2, 4);
+    const f = new Fraction(new Numerator(2), new Denominator(4));
     const actual = f.scaleByDivision(2);
-    const expected = new Fraction(1, 2);
+    const expected = new Fraction(new Numerator(1), new Denominator(2));
     expect(actual).toEqual(expected);
   });
 
   it("Should be able to reduce a fraction", function() {
-    const f = new Fraction(6, 9);
+    const f = new Fraction(new Numerator(6), new Denominator(9));
     const actual = f.reduce();
-    const expected = new Fraction(2, 3);
+    const expected = new Fraction(new Numerator(2), new Denominator(3));
     expect(actual).toEqual(expected);
   });
 
   it("Should be able to add fractions with different denominators and reduce the resulting fraction", function() {
-    const f1 = new Fraction(1, 4);
-    const f2 = new Fraction(1, 4);
+    const f1 = new Fraction(new Numerator(1), new Denominator(4));
+    const f2 = new Fraction(new Numerator(1), new Denominator(4));
     const actual = f1.plus(f2);
-    const expected = new Fraction(1, 2);
+    const expected = new Fraction(new Numerator(1), new Denominator(2));
     expect(actual).toEqual(expected);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,52 @@
-import { gcd } from "./gcd";
+import { gcd as computeGCD } from "./gcd";
+
+class ValueWrapper {
+  constructor(public value: number) {}
+
+  multiply(n: number): ValueWrapper {
+    return new ValueWrapper(this.value * n);
+  }
+
+  divide(n: number): ValueWrapper {
+    return new ValueWrapper(this.value / n);
+  }
+}
+
+export class Numerator extends ValueWrapper {}
+
+export class Denominator extends ValueWrapper {
+  constructor(public value: number) {
+    super(value);
+    if (value === 0) {
+      throw new Error("Cannot create a zero denominator.");
+    }
+  }
+}
 
 export class Fraction {
-  constructor(private numerator: number, private denominator: number) {}
+  constructor(private numerator: Numerator, private denominator: Denominator) {}
 
   scaleByMultiplication(value: number): Fraction {
-    return new Fraction(this.numerator * value, this.denominator * value);
+    return new Fraction(this.numerator.multiply(value), this.denominator.multiply(value));
   }
 
   scaleByDivision(value: number): Fraction {
-    return new Fraction(this.numerator / value, this.denominator / value);
+    return new Fraction(this.numerator.divide(value), this.denominator.divide(value));
+  }
+
+  gcd(): number {
+    return computeGCD(this.numerator.value, this.denominator.value);
   }
 
   reduce(): Fraction {
-    return this.scaleByDivision(gcd(this.numerator, this.denominator));
+    return this.scaleByDivision(this.gcd());
   }
 
   plus(other: Fraction): Fraction {
-    const f1 = this.scaleByMultiplication(other.denominator);
-    const f2 = other.scaleByMultiplication(this.denominator);
-    return new Fraction(f1.numerator + f2.numerator, f1.denominator).reduce();
+    const f1 = this.scaleByMultiplication(other.denominator.value);
+    const f2 = other.scaleByMultiplication(this.denominator.value);
+    const numerator = new Numerator(f1.numerator.value + f2.numerator.value);
+    const denominator = f1.denominator;
+    return new Fraction(numerator, denominator).reduce();
   }
 }
